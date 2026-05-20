5d514e7146926c1171cbbe3f2a02125068d172de
Guiroupe <guiiroupe@gmail.com>
2026-05-20 19:12:37 -0300

    fix: salva ultimos ajustes no repositorio

--- src/main/java/io/github/leonluz/gatewayapi/pedidos/repository/AquisicaoRepository.java
+++ src/main/java/io/github/leonluz/gatewayapi/pedidos/repository/AquisicaoRepository.java
@@ -10,11 +10,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AquisicaoRepository extends JpaRepository<Aquisicao, String> {
 
-    @Modifying
-    @Query(value = "INSERT INTO aquisicao (id_aquisicao, id_usuario, data_aquisicao, status) VALUES (:idAquisicao, :idUsuario, NOW(), 'AGUARDANDO_PAGAMENTO')", nativeQuery = true)
-    void criarAquisicao(@Param("idAquisicao") String idAquisicao, @Param("idUsuario") String idUsuario);
+  @Modifying
+  @Query(value = "INSERT INTO aquisicao (id_aquisicao, id_usuario, data_aquisicao, status) VALUES (:idAquisicao, :idUsuario, NOW(), 'AGUARDANDO_PAGAMENTO')", nativeQuery = true)
+  void criarAquisicao(@Param("idAquisicao") String idAquisicao, @Param("idUsuario") String idUsuario);
 
-    @Modifying
-    @Query(value = "INSERT INTO item_aquisicao (id_item, id_aquisicao, id_patente) VALUES (:idItem, :idAquisicao, :idPatente)", nativeQuery = true)
-    void adicionarItemAquisicao(@Param("idItem") String idItem, @Param("idAquisicao") String idAquisicao, @Param("idPatente") String idPatente);
+  @Modifying
+  @Query(value = "INSERT INTO item_aquisicao (id_item, id_aquisicao, id_patente) VALUES (:idItem, :idAquisicao, :idPatente)", nativeQuery = true)
+  void adicionarItemAquisicao(@Param("idItem") String idItem, @Param("idAquisicao") String idAquisicao,
+      @Param("idPatente") String idPatente);
 }
