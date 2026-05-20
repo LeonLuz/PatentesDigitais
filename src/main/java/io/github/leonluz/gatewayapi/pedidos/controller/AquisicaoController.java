538a6e71566389be0ded14c819df8d61fc999598
Guiroupe <guiiroupe@gmail.com>
2026-05-20 20:16:36 -0300

    feat: subindo novamente

--- src/main/java/io/github/leonluz/gatewayapi/pedidos/controller/AquisicaoController.java
+++ src/main/java/io/github/leonluz/gatewayapi/pedidos/controller/AquisicaoController.java
@@ -1,5 +1,28 @@
 package io.github.leonluz.gatewayapi.pedidos.controller;
 
+<<<<<<< HEAD
+import io.github.leonluz.gatewayapi.pedidos.service.AquisicaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/checkout")
+public class AquisicaoController {
+
+  private final AquisicaoService aquisicaoService;
+
+  public AquisicaoController(AquisicaoService aquisicaoService) {
+    this.aquisicaoService = aquisicaoService;
+  }
+
+  @PostMapping("/{idUsuario}")
+  public ResponseEntity<String> realizarCheckout(@PathVariable String idUsuario) {
+    String idAquisicao = aquisicaoService.finalizarCheckout(idUsuario);
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body("Checkout finalizado com sucesso! ID da Transação: " + idAquisicao);
+  }
+=======
 import io.github.leonluz.gatewayapi.pedidos.dto.AquisicaoRequestDTO;
 import io.github.leonluz.gatewayapi.pedidos.model.Aquisicao;
 import io.github.leonluz.gatewayapi.pedidos.service.AquisicaoService;
@@ -24,4 +47,5 @@ public class AquisicaoController {
     public Aquisicao buscarPorId(@PathVariable String id) {
         return aquisicaoService.buscarPorId(id);
     }
+>>>>>>> main
 }
