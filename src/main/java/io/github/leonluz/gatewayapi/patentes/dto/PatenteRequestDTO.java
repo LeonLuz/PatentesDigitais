5616001ec3c30e4694198f6ba4cceb59e93686fd
Isabel Marinho dos Santos <isabelms2005@gmail.com>
2026-05-25 18:31:41 -0300

    Alterando valor patente para BigDecimal

--- src/main/java/io/github/leonluz/gatewayapi/patentes/dto/PatenteRequestDTO.java
+++ src/main/java/io/github/leonluz/gatewayapi/patentes/dto/PatenteRequestDTO.java
@@ -1,16 +1,17 @@
 package io.github.leonluz.gatewayapi.patentes.dto;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
 public record PatenteRequestDTO(
-        UUID idTitular,
         String titulo,
         String numDeposito,
         String resumo,
         String area,
-        Double valor,
+        BigDecimal valor,
         String pesquisadores,
         String documento,
+        String status, // Adicionado
         List<UUID> idsPesquisadoresAssociados
 ) {}
